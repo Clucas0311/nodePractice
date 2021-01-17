3c956aed84f494d3a97fe1e044bc3578be0ec901
Clucas0311 <clucas0311@gmail.com>
2021-01-17 17:52:52 -0500

    Event Listener Demo

---
+++ reference/fs_demo.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create a file
+fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
+	if (err) throw err;
+	{
+		console.log('Folder Created...');
+	}
+});
+
+// create and write a file to file
+fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World', (err) => {
+	if (err) throw err;
+	{
+		console.log('File written to...');
+	}
+});
+
+// Read File
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+	if (err) throw err;
+	console.log(data);
+});
