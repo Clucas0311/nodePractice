188270de0a01032287941a1c370cffebe92309a7
Clucas0311 <clucas0311@gmail.com>
2021-01-17 17:24:49 -0500

    Using Prexisting Modules

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -3,3 +3,12 @@ const path = require('path');
 
 // Base File Name -- give you the base filename
 console.log(path.basename(__filename));
+
+// Directory name
+console.log(path.dirname(__filename));
+
+// File extension
+console.log(path.extname(__filename));
+
+// Create path object
+console.log(path.parse(__filename).base);
